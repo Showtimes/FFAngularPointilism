57ba68c026f67808f9d3d29328ab2982d7eaa867
James William Graham <wittedhaddock@gmail.com>
2014-08-24 11:51:07 -0400

    Complete tessellation — no gaps

--- TriangularPointilism/FFShutteringViewController.m
+++ TriangularPointilism/FFShutteringViewController.m
@@ -50,30 +50,50 @@
     
     
     
-    NSTimer *timer = [NSTimer timerWithTimeInterval:0.003f target:self selector:@selector(fire:) userInfo:nil repeats:YES];
+    NSTimer *timer = [NSTimer timerWithTimeInterval:0.006f target:self selector:@selector(fire:) userInfo:nil repeats:YES];
     [[NSRunLoop currentRunLoop] addTimer:timer forMode:NSDefaultRunLoopMode];
-    for (int i = 0; i <= self.view.bounds.size.width; i += width/2.0) {
-        for (int j = 0; j <= self.view.bounds.size.height; j+=width/2.0) {
+    for (int i = 0; i <= self.view.bounds.size.width; i += width) {
+        for (int j = 0; j <= self.view.bounds.size.height; j+=width) {
             
             UIImageView *topLeft = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"topLeft"]];
             UIImageView *topRight = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"topRight"]];
             UIImageView *bottomLeft = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"bottomLeft"]];
             UIImageView *bottomRight = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"bottomRight"]];
+            
+            UIImageView *topLeft2 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"topLeft"]];
+            UIImageView *topRight2 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"topRight"]];
+            UIImageView *bottomLeft2 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"bottomLeft"]];
+            UIImageView *bottomRight2 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"bottomRight"]];
 
-
+           
 
             bottomRight.frame = CGRectMake(i, j, topLeft.frame.size.width, topLeft.frame.size.height);
+            topLeft2.frame    = bottomRight.frame;
             bottomLeft.frame = CGRectMake(i + topLeft.frame.size.width, j, topRight.frame.size.width, topRight.frame.size.height);
-            topLeft.frame = CGRectMake(i, j + topLeft.frame.size.height, bottomLeft.frame.size.width, bottomLeft.frame.size.height);
+            topRight2.frame = bottomLeft.frame;
+
+            topRight.frame = CGRectMake(i, j + topLeft.frame.size.height, bottomLeft.frame.size.width, bottomLeft.frame.size.height);
+            bottomLeft2.frame = topRight.frame;
             topLeft.frame = CGRectMake(i + bottomLeft.frame.size.width, j + topLeft.frame.size.height, bottomRight.frame.size.width, bottomRight.frame.size.height);
+            bottomRight2.frame = topLeft.frame;
             topLeft.alpha = [grayscales[arc4random() % grayscales.count] floatValue];
+            topLeft2.alpha = [grayscales[arc4random() % grayscales.count] floatValue];
             topRight.alpha = [grayscales[arc4random() % grayscales.count] floatValue];
+            topRight2.alpha = [grayscales[arc4random() % grayscales.count] floatValue];
             bottomLeft.alpha = [grayscales[arc4random() % grayscales.count] floatValue];
+            bottomLeft2.alpha = [grayscales[arc4random() % grayscales.count] floatValue];
             bottomRight.alpha = [grayscales[arc4random() % grayscales.count] floatValue];
+            bottomRight2.alpha =[grayscales[arc4random() % grayscales.count] floatValue];
+            
+            
             [self.view addSubview:topLeft];
+            [self.view addSubview:topLeft2];
             [self.view addSubview:topRight];
+            [self.view addSubview:topRight2];
             [self.view addSubview:bottomLeft];
+            [self.view addSubview:bottomLeft2];
             [self.view addSubview:bottomRight];
+            [self.view addSubview:bottomRight2];
             
         }
     }
